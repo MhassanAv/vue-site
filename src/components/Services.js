8f7481c15cf066b0b1382471f3e84e2c60e5a6b2
Muhammed Hassan <m.hassananwer@gmail.com>
2023-07-20 22:08:21 +0300

    swiper sections

---
+++ src/components/Services.js
@@ -0,0 +1,13 @@
+import { Center,Heading } from '@chakra-ui/react'
+
+export default function Services() {
+  return (
+      <Center
+      h="100vh"
+      maxH="100vh"
+      w="full"
+      >
+        <Heading>Services</Heading>
+      </Center>
+  )
+}
