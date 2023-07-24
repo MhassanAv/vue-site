a1b245b1c479e05af4b85756fe9cac9352091fa3
Muhammed Hassan <m.hassananwer@gmail.com>
2023-07-24 22:35:58 +0300

    about responsive

--- src/components/About.js
+++ src/components/About.js
@@ -16,29 +16,29 @@ export default function About() {
       h="100dvh"
       maxH="100dvh"
       w="full"
-      py="2rem"
-      flexDir={["column", "column","column", "row"]}
-      gap={'2rem'}
-      justifyContent={["center", "center","center", "space-between"]}
+      py="1rem"
+      flexDir={["column","column","column","row"]}
+      gap={'1rem'}
+      justifyContent={["center", "center", "space-between"]}
       px={['1.5rem','3rem','5rem']}
     >
-      <VStack flex='1' gap="2rem" justifyContent={"center"} alignItems={'center'}>
-        <Heading fontSize={["1.2rem", "1.5rem", "2.5rem"]}>
+      <VStack flex='1' gap="2rem" justifyContent={"center"} alignItems={'center'} w='full'>
+        <Heading fontSize={["3vh","2rem", "2.5rem"]} textAlign={['center','left','left']}>
           We are your one-stop shop for a complete online presence for your
           company or startup.
         </Heading>
-        <Text fontSize={["0.8rem", "1rem"]}>
+        <Text fontSize={["2vh","1rem"]} textAlign={'justify'}>
           We provide a wide array of services including{" "}
           <strong>
             full-stack web development, graphic design, branding, SEO,
             copywriting, mobile development, and digital marketing.
-          </strong>{" "}
+          </strong>
           With VUE, you can find the perfect service package to fit the needs of
           your company or startup with optimal cost, all while balancing cost
           with quality.
         </Text>
         <Button
-          display={['none','none','block']}
+          display={['none','none','none','block']}
           bg='prim'
           rounded={"full"}
           variant={"unstyled"}
@@ -55,7 +55,7 @@ export default function About() {
           Contact Us
         </Button>
       </VStack>
-      <Center flex='1' p='0'  >
+      <Center flex='1' p='0' w={['full','35vh','40vh','full']} >
         <SimpleGrid columns={2} gap={"1rem"} >
           <Box pos="relative">
             <Image src="about/1.png" alt="about"  />
